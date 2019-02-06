bbd33bfd49129745f4cd9ec032d0db615325b5d6
Zack Snyder <zsnyder317@yahoo.com>
2019-02-05 22:45:56 -0600

    ~ Added parse method to Command class

--- core/mesh/command.py
+++ core/mesh/command.py
@@ -1,7 +1,12 @@
 __author__ = "Zack Snyder"
 __date__ = "1/24/19"
 
+import string
+
+from config import Configuration
+
 class Command():
     """Command class defines the types of commands available to transmit"""
-    def __init__(self, ):
-        pass
+
+    def parse(self, command):
+        parts = string.split(command, Configuration.separator)
