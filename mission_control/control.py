23bf729c5c4de52fb45206b23dcf16e8df6b4ac8
seanmclark15 <seanmclark15@gmail.com>
2019-04-02 15:11:23 -0500

    updated control.py

--- mission_control/control.py
+++ mission_control/control.py
@@ -3,10 +3,12 @@ import threading
 
 class UIThread(threading.Thread):
     def __init__(self):
-        exec(open("mission_control/backendexample.py").read())
+        self.runFile()
 
-
-
+    def runFile(self):
+        fileStream = open("mission_control/backendexample.py").read()
+        script = compile(fileStream,"mission_control/backendexample.py","exec")
+        exec(script)
 
 uithread = UIThread()
 uithread.start()
