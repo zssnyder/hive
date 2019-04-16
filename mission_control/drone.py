2a307953bb9fce473c3e8247923f97e7b8dd297b
seanmclark15 <seanmclark15@gmail.com>
2019-04-16 13:28:26 -0500

    updated getsets for drone.py

--- mission_control/drone.py
+++ mission_control/drone.py
@@ -9,4 +9,21 @@ class Drone():
     group = None
     def __init__(self):
 
+    def getX(self):
+        return x
+
+    def getY(self):
+        return y
+
+    def getZ(self):
+        return z
+
+    def setX(self,inX):
+        self.x = inX
+
+    def setY(self,inY):
+        self.y = inY
+        
+    def setZ(self,inZ):
+        self.z = inZ
     
